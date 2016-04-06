bc4478042f39683b0c7d32ca6bfe9553aec6fee8
etmarch <etmarch@gmail.com>
2016-04-06 07:28:16 -0400

    First Commit with new project

--- client/modules/core/routes.jsx
+++ client/modules/core/routes.jsx
@@ -6,6 +6,16 @@ import Home from './components/home.jsx';
 
 import { Accounts } from 'meteor/std:accounts-ui';
 
+Accounts.config({
+  sendVerificationEmail: true,
+  forbidClientAccountCreation: false
+});
+
+Accounts.ui.config({
+  passwordSignupFields: 'USERNAME_AND_OPTIONAL_EMAIL',
+  loginPath: '/login'
+});
+
 export default function (injectDeps, {FlowRouter}) {
   const MainLayoutCtx = injectDeps(MainLayout);
 
